b2ab884ff3abbe3f56b256e1ee821a6d66581be6
IPTVUNION <50663625+IPTVUNION@users.noreply.github.com>
2019-05-18 17:15:01 +0300

    Create iptvunionWeb.sh

---
+++ iptvunionWeb.sh
@@ -0,0 +1,14 @@
+
+mv /home/iptvunion/www/controllers/config.php  /home/iptvunion/config.php 
+sleep 1
+rm -r /home/iptvunion/www/*
+sleep 1
+cd /home/iptvunion/www/
+sleep 1
+wget https://github.com/IPTVUNION/iptvunion/raw/master/web.tar.gz
+sleep 1
+tar -xvzf web.tar.gz
+sleep 1
+rm -r web.tar.gz
+sleep 1
+mv /home/iptvunion/config.php /home/iptvunion/www/controllers/config.php
