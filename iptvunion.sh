14512ffdefb31c649383a9b9212b94b31171e500
IPTVUNION <50663625+IPTVUNION@users.noreply.github.com>
2019-09-30 22:11:09 -0700

    Update iptvunion.sh

--- iptvunion.sh
+++ iptvunion.sh
@@ -84,6 +84,7 @@ echo -n "#";
 (apt-get install -y --force-yes iftop > /dev/null 2>&1);
 (apt-get install -y --force-yes libltdl7 > /dev/null 2>&1);
 (apt-get install -y --force-yes libgeoip-dev > /dev/null 2>&1);
+(apt-get install -y --force-yes lsb-release nscd curl > /dev/null 2>&1);
 echo -n "#";
 (apt-get install -y --force-yes libgconf-2-4 > /dev/null 2>&1);
 if [ "$osrelease" == "19.04" ] ; then 
